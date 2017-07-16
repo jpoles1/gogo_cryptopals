6a64f7541f02840937a278055988c93d1479cc94
Jordan Poles <jpoles1@gmail.com>
2017-07-16 00:03:39 -0400

    Somehow managed to get the XORBreaker working for 1.3!!!

--- set1.go
+++ set1.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	FixedXOR("a", "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 }
 
 //Challenge 1.1
@@ -27,4 +30,40 @@ func FixedXOR(hex1 string, hex2 string) string {
 		bytes3[i] = bytes1[i] ^ bytes2[i]
 	}
 	return hex.EncodeToString(bytes3)
+}
+
+//Challenge 1.3
+func XORBreaker(hash string) (string, string, int) {
+	topkey := ""
+	topplain := ""
+	topscore := 0
+	for i := 0; i < 127; i++ {
+		keychar := strconv.FormatInt(int64(i), 16)
+		key := strings.Repeat(keychar, len(hash)/len(keychar))
+		plaintext, _ := hex.DecodeString(FixedXOR(hash, key))
+		score := ScorePlaintext(plaintext)
+		if score > topscore {
+			topscore = score
+			topkey = keychar
+			topplain = string(plaintext)
+		}
+	}
+	z, _ := hex.DecodeString(topkey)
+	topkey = string(z)
+	return topkey, topplain, topscore
+}
+func BoolToInt(b bool) int {
+	i := 0
+	if b {
+		i = 1
+	}
+	return i
+}
+func ScorePlaintext(plaintext []byte) int {
+	TopEngChar := "ETAOINSHRDLU"
+	var score int = 0
+	for i := 0; i < len(plaintext); i++ {
+		score += BoolToInt(strings.ContainsAny(string(plaintext[i]), TopEngChar))
+	}
+	return score
 }
--- set1_test.go
+++ set1_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test1(t *testing.T) {
 	teststr := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
@@ -16,4 +19,8 @@ func Test2(t *testing.T) {
 	if FixedXOR(str1, str2) != validstr {
 		t.Fail()
 	}
+}
+func Test3(t *testing.T) {
+	hash := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	fmt.Println(XORBreaker(hash))
 }
