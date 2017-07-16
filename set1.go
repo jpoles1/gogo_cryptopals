59420963f2dd8b28669908cfbda677cda897dc82
Jordan Poles <jpoles1@gmail.com>
2017-07-15 23:14:50 -0400

    Polishing up 1.2

--- set1.go
+++ set1.go
@@ -7,16 +7,24 @@ import (
 
 func main() {
 }
+
+//Challenge 1.1
 func HexToBase64(hexval string) string {
 	hexbytes, _ := hex.DecodeString(hexval)
 	return base64.StdEncoding.EncodeToString(hexbytes)
 }
+
+//Challenge 1.2
 func FixedXOR(hex1 string, hex2 string) string {
 	bytes1, _ := hex.DecodeString(hex1)
 	bytes2, _ := hex.DecodeString(hex2)
-	var bytes3 []byte
+	if len(bytes1) != len(bytes2) {
+		panic("FixedXOR cannot handle inputs of different lengths.")
+	}
+	//var bytes3 []byte
+	bytes3 := make([]byte, len(bytes1))
 	for i := 0; i < len(bytes1); i++ {
-		bytes3 = append(bytes3, bytes1[i]^bytes2[i])
+		bytes3[i] = bytes1[i] ^ bytes2[i]
 	}
 	return hex.EncodeToString(bytes3)
 }
